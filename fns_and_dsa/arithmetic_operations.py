9fb0b2ff119ded2e8d67413dd01cb258aa25cbb8
Mutabazi-Fab <fmutabazi9@gmail.com>
2024-07-03 04:16:09 -0700

    Arithmetic operration Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,7 @@
 def perform_operation(num1,num2,operation):
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = string(input("Enter the operation (add, subtract, multiply, divide): "))
     if operation == "add":
        return num1+num2
     elif  operation == "subtract":
